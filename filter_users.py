c75a7990cd647708f759d94af33c35e7e4046a00
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-09-01 17:56:50 +0200

    adjusted the code according to PEP guidelines

--- filter_users.py
+++ filter_users.py
@@ -2,36 +2,57 @@ import json
 
 
 def filter_users_by_name(name):
+    '''
+    Filter users by name from a json file
+    :param name: given user name
+    '''
     with open("users.json", "r") as file:
         users = json.load(file)
 
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
-    for user in filtered_users:
-        print(user)
+    if filtered_users:
+        for user in filtered_users:
+            print(user)
+    else:
+        print(f"No users found with name '{name}'.")
 
 
 def filter_by_age(age):
+    '''
+    Filter users by age from a json file
+    :param age: given age (int)
+    '''
     with open("users.json", "r") as file:
         users = json.load(file)
 
     filtered_users = [user for user in users if "age" in user and user["age"] == age]
 
-    for user in filtered_users:
-        print(user)
+    if filtered_users:
+        for user in filtered_users:
+            print(user)
+    else:
+        print(f"No users found with age {age}.")
 
 
 def filter_by_email(email):
+    '''
+    Filter users by email from a json file
+    :param email: given email
+    '''
     with open("users.json", "r") as file:
         users = json.load(file)
 
     filtered_users = [user for user in users if "email" in user and user["email"].lower() == email.lower()]
 
-    for user in filtered_users:
-        print(user)
+    if filtered_users:
+        for user in filtered_users:
+            print(user)
+    else:
+        print(f"No users found with email '{email}'.")
 
 
-if __name__ == "__main__":
+def main():
     filter_option = input("What would you like to filter by? (Currently, 'name', 'age', and 'email' are supported): ").strip().lower()
 
     if filter_option == "name":
@@ -48,3 +69,7 @@ if __name__ == "__main__":
         filter_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
+
+
+if __name__ == "__main__":
+    main()
